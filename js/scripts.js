dca0819d9214bfd40c834501a789f53c10e58030
Jonathan Lu <jdlu.coding@gmail.com>
2022-06-01 10:25:12 -0700

    add additional clickable text to hide show image

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
+    $("#text-hidden").toggle();
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
+    $("#text-showing").toggle();
   });
 });
