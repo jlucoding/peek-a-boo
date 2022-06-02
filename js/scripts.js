6f1b193b92799c0e9d757b5f66aa1a9fe4596a81
Jonathan Lu <jdlu.coding@gmail.com>
2022-06-01 22:06:21 -0700

    switch fadeToggle back to toggle for the image and texts for better visual

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-        $("#text-hidden").fadeToggle();
+    $("#text-hidden").fadeToggle();
     $("#walrus-showing").fadeToggle();
     $("#walrus-hidden").fadeToggle();
     $("#text-showing").fadeToggle();
   });
+
   
   $("button#dark").click(function() {
+    $("button#dark").toggle();
+    $("button#light").toggle();
     $("body").removeClass();
     $("body").addClass("dark-background");
   });
 
   $("button#light").click(function() {
+    $("button#light").toggle();
+    $("button#dark").toggle();
     $("body").removeClass();
     $("body").addClass("light-background");
   });
