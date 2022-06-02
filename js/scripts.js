572f990e5c2ee161a567911c2dabacf18fde01d8
Jonathan Lu <jdlu.coding@gmail.com>
2022-06-01 21:33:51 -0700

    add two toggle theme buttons to switch dark/light mode and add jQuery methods and css rules to apply background color changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#text-hidden").fadeToggle();
+        $("#text-hidden").fadeToggle();
     $("#walrus-showing").fadeToggle();
     $("#walrus-hidden").fadeToggle();
     $("#text-showing").fadeToggle();
   });
-});
+  
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
 
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+});
